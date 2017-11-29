b51f6a737da2c135ea4a1ad35ba91bedaa6643a8
Paul <pdent0075@aol.com>
2017-11-28 21:26:09 -0500

    project 2

---
+++ Project 2/views/barber.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS barbershop;
+USE barbershop;
+DROP TABLE IF EXISTS Products;
+
+CREATE TABLE Products (
+    ItemId int(11) AUTO_INCREMENT NOT NULL,
+    ProductName varchar(100) NOT NULL,
+    StockQuantity  int NOT NULL,
+    PRIMARY KEY (ItemId)
+);
+
+INSERT INTO Products (ItemId, ProductName, StockQuantity)
+VALUES (1, 'Shampoo', 210);
+
+INSERT INTO Products (ItemId, ProductName, StockQuantity)
+VALUES (2, 'Conditioner', 219);
+
+INSERT INTO Products (ItemId, ProductName, StockQuantity)
+VALUES (3, 'Brushes', 319);
+
+INSERT INTO Products (ItemId, ProductName, StockQuantity)
+VALUES (4 ,'Sponges', 110);
+
+INSERT INTO Products (ItemId, ProductName, StockQuantity)
+VALUES (5, 'Brushes', 291);
